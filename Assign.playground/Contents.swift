b4c11070ffc623b77726dab53b6712b792ccda2a
Monse <monsetorres@gmail.com>
2016-08-21 21:31:08 -0500

    Assignment 1

---
+++ Assign.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var rg = 0...100
+
+for i in rg{
+    //print("valor \(i) \n")
+    if i%5==0{
+        print("#\(i) + Bingo!!!\n")
+    }
+    if i%2==0{
+        print("#\(i) + es Par!!!\n")
+    }else{
+        print("#\(i) + es Impar!!!\n")
+    }
+    if(i>=30)&&(i<=40){
+        print("#\(i) + Viva Swift!!!!!!\n")
+    }
+}
